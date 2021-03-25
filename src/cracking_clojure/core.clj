5dc9aed0f3e1d4d31aa13d96755a9d1acfd89004
Cara <cararbrennan@gmail.com>
2021-03-24 18:08:47 -0700

    strings

--- src/cracking_clojure/core.clj
+++ src/cracking_clojure/core.clj
@@ -5,3 +5,18 @@
   "I don't do a whole lot ... yet."
   [& args]
   (println "Hello, World!"))
+  
+(defn isUnique
+  [args]
+  (= (count args) (count(distinct args)))
+  )
+
+(defn checkPermutation [args args2]
+(= (sort args) (sort args2))
+
+)
+
+(defn insert20s [args ]
+
+
+(clojure.string/join "%20" (clojure.string/split args #"\s")))
